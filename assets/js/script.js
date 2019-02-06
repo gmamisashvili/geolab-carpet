bf03cf8f421dc75d43b114cf7432b716abeca59b
Perfstranger <sali.kachibaia@gmail.com>
2019-02-06 20:03:10 +0400

    validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,19 @@ $(function() {
     let heightOfHeader = $(".header").outerHeight();
     $("body").css("marginTop", heightOfHeader);
   });
+
+  let inputs= [$('.name-input'), $('.email-input'), $('.satauri-input'), $('.textarea-input')];
+  let errors= ['ჩაწერეთ სახელი', 'ჩაწერეთ მეილი', 'ჩაწერეთ სათაური', 'ჩაწერეთ ტექსტი'];
+
+  $('.submit-btn').on('click',function(e){
+  for (i=0;i<inputs.length;i++){
+    if (inputs[i].val()==""){
+      inputs[i].attr('placeholder',errors[i]);
+      inputs[i].css('border-color','red');
+      e.preventDefault();
+    }
+    else{
+      inputs[i].css('border-color','black');
+    }
+  }})
 });
